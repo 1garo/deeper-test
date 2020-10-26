4b807cd64a4b95cf1de2257ee9e64b988a7342cd
1garo <alevardai@hotmail.com>
2020-10-26 15:13:10 -0300

    feat: python solution to problem

---
+++ main.py
@@ -0,0 +1,39 @@
+# Very important here - the lower the interger, the higher the priority
+# 1 higher
+# 100 lower
+import json
+from collections import OrderedDict
+
+with open('source_file_2.json', 'r') as myfile:
+    data=myfile.read()
+
+obj = json.loads(data)
+ord_obj = OrderedDict()
+ord_obj  = obj
+ordered_object = sorted(ord_obj, key=lambda x: x["priority"])
+# print(ordered_object)
+# print(type(OrderedDict(obj[0])))
+res_managers = {} 
+res_watchers = {}
+for i in ordered_object:
+    res_managers.update({
+        i["name"]: i["managers"]
+    })
+    res_watchers.update({
+        i["name"]: i["watchers"]
+    })
+
+  # print("{}\n".format(i["name"]))
+
+# a = {
+#     i.name: managers
+# }
+
+# b = {
+#     i["name"]: watchers 
+# }
+with open("managers.json", "w") as fout: 
+    json.dump(res_managers, fout, indent=4)
+
+with open("watchers.json", "w") as fout: 
+    json.dump(res_watchers, fout, indent=4)
